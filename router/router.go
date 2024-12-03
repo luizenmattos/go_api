ac2c0b4c8dd0d6037bc81cee7902a995f9d3c40c
luiz <lhamattos@outlook.com>
2024-12-03 19:56:43 -0300

    bug: removing a rout

--- router/router.go
+++ router/router.go
@@ -9,11 +9,5 @@ func Initialize() {
 	//Initialize routes
 	initializeRoutes(r)
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
 	r.Run(":8080")
 }
