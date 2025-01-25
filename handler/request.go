0ab9814711d8d77e3bbb1e99cacf288a200b67ce
luiz <lhamattos@outlook.com>
2025-01-25 16:24:07 -0300

    feat: add update request struc

--- handler/request.go
+++ handler/request.go
@@ -40,3 +40,20 @@ func (r *CreateOpeningRequest) Validate() error {
 func errParamIsRequired(name, typ string) error {
 	return fmt.Errorf("param %s (type: %s) is required", name, typ)
 }
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one valid field must be provided")
+}
