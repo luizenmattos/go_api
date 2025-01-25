039c5d1a5c25e41fc5ba0a1fbac05026513a3fd2
luiz <lhamattos@outlook.com>
2025-01-25 16:22:48 -0300

    feat: add get logic

--- handler/handler.go
+++ handler/handler.go
@@ -50,9 +50,21 @@ func CreateOpeningHandler(ctx *gin.Context) {
 }
 
 func GetOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "get-opening", &opening)
 }
 
 func DeleteOpeningHandler(ctx *gin.Context) {
